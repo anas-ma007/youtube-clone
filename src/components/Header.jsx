2a1ca8183f00504698ae696ec20b09b704019805
anas-ma007 <anaschelakkara786@gmail.com>
2023-11-03 03:32:44 +0530

    search icon bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
                 </div>
                 <div className="flex col-span-9 justify-center">
                     <input type="text"  className="rounded-l-full w-1/2 h-14 my-auto border border-gray-400 border-r-0 pl-3 " placeholder="Search" /> <button className="my-auto h-14 bg-slate-200 rounded-r-full border border-gray-400 px-5">
-                        <img className="h-7 my-auto" src="../../public/pngwing.com.png" alt="s-icon" /> </button>
+                        <img className="h-7 my-auto" src="src\assets\pngwing.com.png" alt="s-icon" /> </button>
                     <img className="h-7 mx-7 my-auto" src="https://freesvg.org/img/miciconmuted.png" alt="voice" />
                 </div>
                 <div className="flex col-span-1.5">
